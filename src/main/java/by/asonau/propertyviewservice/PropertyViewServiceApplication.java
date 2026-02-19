28c38f7633f5de0ac41085d347f4ed89c187ed2f
Maksim Asonov <zabungalilovo@gmail.com>
2026-02-19 13:35:16 +0300

    config: set port and context-path, add postgres profile for future quick change

---
+++ src/main/java/by/asonau/propertyviewservice/PropertyViewServiceApplication.java
@@ -0,0 +1,13 @@
+package by.asonau.propertyviewservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyViewServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyViewServiceApplication.class, args);
+	}
+
+}
